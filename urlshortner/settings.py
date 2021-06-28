e83ccd2e44488480524bcae5a3e45b1a0a019563
Abhishek Waghmare <49281527+abhiwaghmare@users.noreply.github.com>
2021-06-28 20:02:19 +0530

    output changes1

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite://db/sqlite3.db')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
